e18c02ae18531631d4e4dab716edc416949334df
Barskyi <137706923+Barskyi@users.noreply.github.com>
2023-10-21 00:01:30 +0300

    Update clean.py

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -1,127 +1,100 @@
-import os
-import sys
 from pathlib import Path
-import glob
+import shutil
 
+pictures = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'heic', 'tiff', 'ico', 'webp', 'JPG']
+movie = ['mp4', 'avi', 'mov', 'mkv', 'flv', 'wmv', 'webm', '3gp', 'm4v', 'ts', 'ogg']
+documents = ['pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'rtf']
+audio = ['mp3', 'wav', 'flac', 'aac', 'ogg', 'wma', 'm4a']
+text_documents = ['txt', 'rtf', 'odt',  'md']
+achives = ["zip", "gz", "tar", "dmg"]
+python_learn = ["ipynb"]
+trying_movie = []
 
-""" Створюємо список для файлів та папок які нас цікавлять """
+class Way:
+    def __init__(self, path):
+        self.path = Path(path)
 
-pictures = list()
-video = list()
-doc = list()
-sound = list()
-archive_files = list()
-other_files = list()  # не можемо визначити розширення
-folders = list('pictures' 'video' 'doc' 'sound' 'archive_files')  # папки
-unknowns = set()  # невідомі файли
-extensions = set()  # відомі файли з якими ми працюємо
+    def recursive_func(self, recursive):
 
-register_extension = {
-    "JPEG": pictures,
-    "PNJ": pictures,
-    "JPL": pictures,
-    "SVG": pictures,
-    "AVI": video,
-    "MP4": video,
-    "MOV": video,
-    "MKV": video,
-    "DOC": doc,
-    "DOCX": doc,
-    "TXT": doc,
-    "PDF": doc,
-    "XLSX": doc,
-    "PPTX": doc,
-    "MP3": sound,
-    "OGG": sound,
-    "WAV": sound,
-    "AMR": sound,
-    "ZIP": archive_files,
-    "GZ": archive_files,
-    "TAR": archive_files,
+        for i in recursive.iterdir():  # проходимось по всіх файлах у заданій директорії
 
-}
-
-def sorted_file(folder_path):
-    """ Sorted our file into list and folders """
-    for el in folder_path.iterdir():
-        if el.is_file():
-            for key in register_extension.keys():
-                if str(el.suffix.replace('.', '')) in register_extension.keys():
-                    other_files.append(el)
-                    if str(el.suffix.replace('.', '')) == key:
-                        register_extension[key].add(str(el.suffix.replace('.', '')))
+            if i.is_dir():  # якщо це директорія
+                self.recursive_func(i)  # викликаємо рекурсію що б ввійти в середину
+            elif i.is_file():  # якщо це файл а не папка
+                if i.suffix[1:].lower() in pictures:  # якщо фото
+                    self.photo(i)
+                elif i.suffix[1:].lower() in movie:  # якщо відео
+                    self.movie(i)
+                elif i.suffix[1:].lower() in documents:
+                    self.documents(i)
+                elif i.suffix[1:].lower() in audio:
+                    self.audio(i)
+                elif i.suffix[1:].lower() in text_documents:
+                    self.text(i)
+                elif i.suffix[1:].lower() in achives:
+                    self.achivs(i)
+                elif i.suffix[1:].lower() in python_learn:
+                    self.python_files(i)
                 else:
-                    unknowns.add(str(el.suffix.replace('.', '')))
-        else:
-            sorted_file(el)
+                    self.unknown(i)
+
+    def photo(self, file):  # file приймає шлях з recursive_func
+        work_photo = self.path / 'WORK_IMAGES'  # назва  нової дерикторії яку ми створюєм
+        work_photo.mkdir(parents=True, exist_ok=True)  # для коректності даних
+        shutil.move(file, work_photo / file.name)
+
+    def movie(self, file):
+        movies = self.path / 'MOVIE'
+        movies.mkdir(parents=True, exist_ok=True)
+        shutil.move(file, movies / file.name)
+        trying_movie.append(str(file.name))
+
+    def documents(self, file):
+        document = self.path / "DOC - FILES"
+        document.mkdir(parents=True, exist_ok=True)
+        shutil.move(file, document / file.name)
+
+    def audio(self, file):
+        music = self.path / "MUSIC"
+        music.mkdir(parents=True, exist_ok=True)
+        shutil.move(file, music / file.name)  # file - це файл який до нас приходить з метода recursive_func
+
+    def text(self, file):
+        texts = self.path / "TEXTS"
+        texts.mkdir(parents=True, exist_ok=True)
+        shutil.move(file, texts / file.name)
+
+    def unknown(self, files):
+        unknowns = self.path / "UNKNOWN"
+        unknowns.mkdir(parents=True, exist_ok=True)
+        shutil.move(files, unknowns / files.name)
+
+    def achivs(self, files):
+        achive = self.path / "ARCHIVE"
+        achive.mkdir(parents=True, exist_ok=True)
+        shutil.move(files, achive / files.name)
+
+    def python_files(self, files):
+        pyth = self.path / "Python-Learn"
+        pyth.mkdir(parents=True, exist_ok=True)
+        shutil.move(files, pyth / files.name)
+
+    def delete_empty_folders_recursive(self):
+        for item in self.path.iterdir():
+            if item.is_dir():
+                subfolder = Way(item)
+                subfolder.delete_empty_folders_recursive()
+        if not any(self.path.iterdir()):
+            try:
+                self.path.rmdir()
+            except OSError:
+                pass
+
+def main():
+    way = Way("/Users/olenka1/Downloads")
+    way.recursive_func(way.path)
+    way.delete_empty_folders_recursive()
 
 
-UKRAINIAN_SYMBOLS = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
-TRANSLATION = (
-    "a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t",
-    "u",
-    "f", "h", "ts", "ch", "sh", "sch", "", "ju", "ja")
-
-TRANS = {}
-
-for key, value in zip(UKRAINIAN_SYMBOLS, TRANSLATION):
-    """ Підміна літер """
-
-    TRANS[ord(key)] = value  # for little word
-    TRANS[ord(key.upper())] = value.upper()  # for big word
-
-
-def normalize(name):  # все що не невідоме заміняємо на " __ "
-
-    """ Підміна символів """
-    new_file_name = name.translate(TRANS)
-    other_files_name = ''
-    for i in new_file_name:
-        if i.isalnum() or i == '.':
-            other_files_name += i
-        else:
-            i = "_"
-            other_files_name += i
-    return other_files_name
-
-
-def scan_file(folder_path):
-    """ here we scan the files and  add them to the list """
-    path = str(folder_path)
-    for el in register_extension.keys():
-        for file in glob(path + f'\\**\\*.{el}', recursive=True):
-            register_extension[el].append(file)
-            extensions.add(f'{el}')
-        for file in glob(path + '\\**\\*.*', recursive=True):
-            if not str(file.endswith(str(el))):
-                other_files.append(file)
-                unknowns.add(file[file.rindex('.') + 1:])
-
-
-def empty_dir(path):
-    try:
-        for el in os.listdir(path):
-            q = os.path.join(path, el)
-            if os.path.isdir(q):
-                empty_dir(q)
-                if not os.listdir(q):
-                    os.rmdir(q)
-    except PermissionError:
-        print("do re mi do re do - sol fa mi re do !")
-
-
-def main(folder_path):
-    path = folder_path
-    sorted_file(path)
-    empty_dir(path)
-
-
-def search_path():
-    path = sys.argv[1]
-    arg = Path(path)
-    main((arg.resolve()))
-
-
-if __name__ == '__main__':
-    search_path()
-
+if __name__=="__main__":
+    main()
